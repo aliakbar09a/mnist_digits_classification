af6850bc69c6b24a1502adde6f19b5b3a13cc801
aliakbar09a <aliakbar09a@gmail.com>
2018-07-18 20:58:11 +0530

    convolutional neural network model added

--- conv_network.py
+++ conv_network.py
@@ -2,23 +2,15 @@
 from tensorflow.python.keras.layers import Dense, Flatten, Conv2D
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python import keras
-
+import numpy as np
 
 num_classes = 10
 img_rows, img_cols = 28, 28
+seed = 7
+np.random.seed(seed)
 
 
-def prep(data, batch_size):
-    raw = data.train.next_batch(batch_size)
-    x = raw[0]
-    y = raw[1]
-    out_y = keras.utils.to_categorical(y, num_classes)
-    out_x = x.reshape(batch_size, img_rows, img_cols, 1)
-    out_x = out_x/255
-    return out_x, out_y
-
-
-def convolution_model(train_x, train_y):
+def model(train_x, train_y, test_x, test_y, epoch):
     conv_model = Sequential()
     # first layer with input shape (img_rows, img_cols, 1) and 12 filters
     conv_model.add(Conv2D(12, kernel_size=(3, 3), activation='relu',
@@ -35,10 +27,17 @@ def convolution_model(train_x, train_y):
     conv_model.add(Dense(num_classes, activation='softmax'))
 
     # compile the model
-    conv_model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
+    conv_model.compile(optimizer=keras.optimizers.Adadelta(),
+                       loss='categorical_crossentropy',
                        metrics=['accuracy'])
-
+    print("\n Training the Convolution neural network on MNIST data\n")
     # fit the model
-    conv_model.fit(train_x, train_y, batch_size=100, epochs=4,
-                   validation_split=0.2)
-    return conv_model
+    conv_model.fit(train_x, train_y, batch_size=128, epochs=epoch,
+                   validation_split=0.1, verbose=2)
+    predicted_train_y = conv_model.predict(train_x)
+    print('Train accuracy : ', (sum(np.argmax(predicted_train_y, axis=1)
+                                    == np.argmax(train_y, axis=1))/(float(len(train_y)))))
+    predicted_test_y = conv_model.predict(test_x)
+    print('Test accuracy : ', (sum(np.argmax(predicted_test_y, axis=1)
+                                   == np.argmax(test_y, axis=1))/(float(len(test_y)))))
+    return conv_model, predicted_train_y, predicted_test_y
